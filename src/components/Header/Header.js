cf6b2b52f61ecc5491b94fb515ab075baa5f0163
Romain KOENIG <romain.koenig@gmail.com>
2020-07-30 00:47:51 +0200

    Creating reusable components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+import Link from '../Link/Link';
+
+const Header = (props) => (
+  <header className="App-header">
+    <img src={props.logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+  </p>
+    <Link url={"https://reactjs.org"} title={"Learn React"} />
+  </header >
+);
+export default Header;
