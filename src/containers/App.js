07b430fae1521f7463a319539be3b761d6cf5a7e
Romain KOENIG <romain.koenig@gmail.com>
2020-07-30 01:02:02 +0200

    Getting data from GitHub API

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
+
 import Header from '../components/Header/Header';
+import Profile from './Profile';
+
 
 function App() {
   return (
     <div className="App">
       <Header logo = {logo}/>
+      <Profile/>
     </div>
   );
 }
